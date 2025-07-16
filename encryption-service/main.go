ed27653d1ec5e0906fc21a8af39639e36597fd24
maulanailhamota <mast.ilham02@gmail.com>
2025-07-16 23:55:51 +0700

    Add files via upload

---
+++ encryption-service/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "os"
+    "net/http"
+    "github.com/joho/godotenv"
+)
+
+func main() {
+    // Load .env
+    err := godotenv.Load()
+    if err != nil {
+        log.Println("Tidak bisa memuat file .env, pakai default environment.")
+    }
+
+    ownerName := os.Getenv("OWNER_NAME")
+    if ownerName == "" {
+        ownerName = "Unknown Owner"
+    }
+
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    fmt.Printf("%s (%s) is running on port %s...\n", os.Getenv("SERVICE_NAME"), ownerName, port)
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "%s Service - Owner: %s", os.Getenv("SERVICE_NAME"), ownerName)
+    })
+
+    log.Fatal(http.ListenAndServe(":" + port, nil))
+}
